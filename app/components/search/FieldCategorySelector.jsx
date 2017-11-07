dd3e302c9098f5ca88036c1eefbd9fe453a07589
Eduardo Silva Navarrete <e.y.silvanavarrete@europeana.eu>
2017-11-07 13:58:46 +0100

    #147 Add multiselection for fields in cluster selector. Work on progress

--- app/components/search/FieldCategorySelector.jsx
+++ app/components/search/FieldCategorySelector.jsx
@@ -1,48 +1,70 @@
 import ElasticsearchDataUtil from '../../util/ElasticsearchDataUtil';
 import ReactTooltip from 'react-tooltip'; //https://www.npmjs.com/package/react-tooltip
 import IDUtil from '../../util/IDUtil';
+import { PowerSelectMultiple } from 'react-power-select';
 
 //TODO this component is not used yet and does not have a proper component ID yet
 class FieldCategorySelector extends React.Component {
 
 	constructor(props) {
 		super(props);
-	}
-
-	changeStringField(e) {
-		this.onOutput(e.target.value == 'null_option' ? null : e.target.value);
+        this.state = {
+            selectedFields: [],
+        };
+        this.handleChange = this.handleChange.bind(this)
 	}
 
 	onOutput(data) {
 		if(this.props.onOutput) {
-			if(data == null) {
+			if(data === null) {
 				this.props.onOutput(this.constructor.name, null);
 			} else {
 				const fieldCategories = this.props.collectionConfig.getMetadataFieldCategories();
-				const fc = fieldCategories.filter((c) => {
-					return c.id == data;
-				})
-				if(fc.length == 1) {
-					this.props.onOutput(this.constructor.name, fc[0]);
-				}
+                const selectedObjects = [];
+
+				data.map(function(current) {
+                    fieldCategories.map(function (obj) {
+						if (obj.label === current) {
+                            selectedObjects.push(obj);
+						}
+						return selectedObjects;
+                    });
+				});
+				console.log('selectedObjects', selectedObjects);
+                this.props.onOutput(this.constructor.name, selectedObjects);
 			}
 		}
 	}
 
+    handleChange ({ options }) {
+		const optionsSelected = options;
+
+		this.onOutput(optionsSelected);
+        this.setState({
+            selectedFields: options
+        });
+    }
+
 	render() {
 		let fieldCategorySelector = null;
-		let includedFields = 'All metadata fields (classified as text field) are included in your search';
-		if(this.props.fieldCategory) {
-			includedFields = 'The following metadata fields are included in this category:<br/><br/>';
-			includedFields += this.props.fieldCategory.fields.map(
-				(f) => this.props.collectionConfig.toPrettyFieldName(f)
-			).join('<br/>');
-		}
+		const includedFields = 'All metadata fields (classified as text field) are included in your search';
+
+		// TODO: Assign tooltip info per selected item instead of the whole selection field now that we are dealing with multiple selections
+		// if(this.props.fieldCategory) {
+        //
+		// 	includedFields = 'The following metadata fields are included in this category:<br/><br/>';
+        //
+        //
+		// 	includedFields += this.props.fieldCategory.fields.map(
+		// 		(f) => this.props.collectionConfig.toPrettyFieldName(f)
+		// 	).join('<br/>');
+		// }
 		if(this.props.collectionConfig.getMetadataFieldCategories()) {
 			const options = this.props.collectionConfig.getMetadataFieldCategories().map((fc) => {
 				return (<option value={fc.id}>{fc.label}</option>);
 			});
-			options.splice(0,0, <option value="null_option">Search in: all fields</option>)
+			const optionsArray = this.props.collectionConfig.getMetadataFieldCategoriesTitles();
+
 			fieldCategorySelector = (
 				<div className={IDUtil.cssClassName('field-category-selector')}>
 				<div className="input-group">
@@ -52,19 +74,21 @@ class FieldCategorySelector extends React.Component {
 						data-html={true}>
 						<i className="fa fa-info"></i>
 					</span>
-					<select className="form-control"
-						value={this.props.fieldCategory ? this.props.fieldCategory.id : null}
-						onChange={this.changeStringField.bind(this)}>
-						{options}
-					</select>
+					<PowerSelectMultiple
+						options={optionsArray}
+						selected={this.state.selectedFields}
+						onChange={this.handleChange}
+						placeholder="Search in: all fields"
+					/>
+
 				</div>
+					{/* TODO: re-locate tooltip on selection multi selection list*/}
 				<ReactTooltip id={'__fs__tt' + this.props.queryId} />
 			</div>);
 		}
 
 		return fieldCategorySelector;
 	}
-
 }
 
 export default FieldCategorySelector;
