baa3336ddd935d155c7804e55e08b4b5ff5472c7
Jaap Blom <jblom@beeldengeluid.nl>
2017-11-29 10:02:02 +0100

    added example collection config + mapping

---
+++ app/collection/mappings/ExampleCollectionMapping.js
@@ -0,0 +1,8 @@
+import ExampleCollectionConfig from './ExampleCollectionConfig'
+
+const ExampleCollectionMapping = {
+
+	'example-index': ExampleCollectionConfig
+}
+
+export default ExampleCollectionMapping;
